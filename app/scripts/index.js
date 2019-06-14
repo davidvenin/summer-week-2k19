5b5981970f01e0eda1b710478f1d7785e83f6d4c
davidvenin <david@venin.pro>
2019-06-14 23:16:44 +0200

    updates icon with animation and form fields required

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,8 +1,32 @@
 import 'normalize.css';
 import 'flexboxgrid';
 import AOS from 'aos';
+import lottie from 'lottie-web';
 import 'aos/dist/aos.css';
 
+import youtube from '../assets/animations/youtube.json';
+import facebook from '../assets/animations/facebook.json';
+import twitter from '../assets/animations/twitter.json';
+import linkedin from '../assets/animations/linkedin.json';
+
 document.addEventListener('DOMContentLoaded', () => {
+  const socials = [
+    {name: 'youtube', data: youtube},
+    {name: 'facebook', data: facebook},
+    {name: 'linkedin', data: linkedin},
+    {name: 'twitter', data: twitter},
+  ];
   AOS.init();
+
+  for(let i = 0; i < socials.length; i++) {
+    console.log(socials[i]);
+    lottie.loadAnimation({
+      container: document.querySelector(`#${socials[i].name}`), // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: socials[i].data, // the path to the animation json
+    });
+  }
+
 });
